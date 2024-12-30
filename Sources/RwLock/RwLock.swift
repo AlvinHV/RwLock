31fbfdf803bb3fcde7c1c3f89c8575f258eff59a
AlvinHV <AlvinHV@proton.me>
2024-12-30 23:40:44 +0400

    First commit

--- Sources/RwLock/RwLock.swift
+++ Sources/RwLock/RwLock.swift
@@ -1,2 +1,19 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+import Foundation
+
+final class RwLock<T> : @unchecked Sendable {
+    private let lock = NSLock()
+    private var resource: T
+    
+    init(resource: T) {
+        self.resource = resource
+    }
+    
+    /// Do not store object itself. Otherwise we cannot ensure thread safety.
+    func access<U>(_ modify: (inout T) -> U) -> U {
+        lock.lock()
+        defer { lock.unlock() }
+        return modify(&resource)
+    }
+}
--- Tests/RwLockTests/RwLockTests.swift
+++ Tests/RwLockTests/RwLockTests.swift
@@ -1,6 +1,38 @@
 import Testing
+import XCTest
 @testable import RwLock
 
-@Test func example() async throws {
-    // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+@Test func testRwLock() throws {
+    let lock = RwLock(resource: [1, 2, 3])
+    
+    lock.access { resource in
+        resource.append(4)
+    }
+    
+    let result = lock.access { $0 }
+    #expect(result == [1, 2, 3, 4])
+}
+
+@Test func testRwLockAsync() throws {
+    let lock = RwLock(resource: 0)
+    let dispatchGroup = DispatchGroup()
+    let max = 1000
+
+    dispatchGroup.enter()
+    for _ in 1...max {
+        DispatchQueue.global().async {
+            lock.access { resource in
+                resource += 1
+                if resource == max {
+                    dispatchGroup.leave()
+                }
+            }
+        }
+    }
+    
+    dispatchGroup.wait()
+
+    lock.access { result in
+        #expect(result == max, "Resource should be incremented correctly by all tasks")
+    }
 }
